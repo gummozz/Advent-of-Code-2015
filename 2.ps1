c86480f31a21d7129b1e110c87e174239a76e1be
gummozz <melkerssehlstedt@gmail.com>
2016-12-05 21:23:37 +0100

    Update and rename 2-1.ps1 to 2.ps1

--- 2.ps1
+++ 2.ps1
@@ -1,6 +1,6 @@
 $instructions = "" -split '\n'
 $answer = 0
-
+$ribbon = 0
 foreach ($instruction in $instructions) {
 
     [int]$l = $instruction.split("x")[0]
@@ -16,7 +16,10 @@ foreach ($instruction in $instructions) {
     $ah = (2*$h*$l)
 
     $answer += $al + $aw + $ah + ([int]($l2,$w2,$h2 | Sort-Object | select -First 1))
+    $l,$w,$h | Sort-Object | select -First 2 | % {$ribbon += (2*$_)}
+    $ribbon += $l*$w*$h
     
 }
 
 $answer
+$ribbon
