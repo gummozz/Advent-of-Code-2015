2f5415a031e2f8808d6b5af1db78dff493994003
gummozz <melkerssehlstedt@gmail.com>
2016-12-05 21:17:13 +0100

    Create 2-1.ps1

---
+++ 2-1.ps1
@@ -0,0 +1,22 @@
+$instructions = "" -split '\n'
+$answer = 0
+
+foreach ($instruction in $instructions) {
+
+    [int]$l = $instruction.split("x")[0]
+    [int]$w = $instruction.split("x")[1]
+    [int]$h = $instruction.split("x")[2]
+    
+    $l2 = ($l*$w)
+    $w2 = ($w*$h)
+    $h2 = ($h*$l)
+
+    $al = (2*$l*$w)
+    $aw = (2*$w*$h)
+    $ah = (2*$h*$l)
+
+    $answer += $al + $aw + $ah + ([int]($l2,$w2,$h2 | Sort-Object | select -First 1))
+    
+}
+
+$answer
